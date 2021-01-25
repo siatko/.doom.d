9eabca72cdcc9ce728379b4a7c8c43d0066a7af6
Simon Weis <simonattilaweis@tutanota.com>
2021-01-25 08:53:20 +0100

    Emacs: Update org-journal type

--- init.el
+++ init.el
@@ -75,11 +75,11 @@
        eshell                 ; the elisp shell that works everywhere
        ;;shell                ; simple shell REPL for Emacs
        ;;term                 ; basic terminal emulator for Emacs
-       vterm                ; the best terminal emulation in Emacs
+       vterm                  ; the best terminal emulation in Emacs
 
        :checkers
        ;;syntax               ; tasing you for every semicolon you forget
-       ;;spell                ; tasing you for misspelling mispelling
+       ;;(spell +aspell)        ; tasing you for misspelling mispelling
        ;;grammar              ; tasing grammar mistake every you make
 
        :tools
